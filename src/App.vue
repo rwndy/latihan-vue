2682f379d925fdf32344a1bc221b96286ff27348
riwandi <riwandy10@gmail.com>
2021-02-18 16:08:57 +0700

    first blood

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyName name="My Name is Riwandi" age="25"/>
+  <p>my favourite game is:</p>
+  <ul>
+    <li v-for="list in lists" :key="list.id">{{ list.name }}</li>
+  </ul>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MyName from './components/MyName'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyName
+  },
+  data() {
+    return {
+      lists: [
+        {
+          id: 1,
+          name: 'Dota 2',
+        },
+        {
+          id: 2,
+          name: 'RE7'
+        }
+      ]
+    }
   }
 }
 </script>
@@ -22,5 +43,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+li {
+  list-style-type: none;
 }
 </style>
---
+++ src/components/MyName.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <h1>{{ name }}</h1>
+    <h4>{{ age }}</h4>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'MyName',
+  props: {
+    name: String,
+    age: Number,
+  },
+  
+}
+</script>
